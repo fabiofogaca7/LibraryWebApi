e88e098b1ca751bd2c5c8adde5fe9bf0931af009
Fabio Fogaça <fabiofogaca7@hotmail.com>
2025-04-27 14:03:32 -0300

    Revert "Books added for test adjustment"
    This reverts commit a5ab64dac31ded3ba42de94eb661051965dac301.

--- Infrastructure/Data/LibraryContext.cs
+++ Infrastructure/Data/LibraryContext.cs
@@ -18,9 +18,9 @@ public class LibraryContext : DbContext
             new Book { Id = 1, Title = "Dom Quixote", Author = "Miguel de Cervantes", CreatedDate = DateTime.Now.AddMonths(-6) },
             new Book { Id = 2, Title = "Cem Anos de Solidão", Author = "Gabriel García Márquez", CreatedDate = DateTime.Now.AddMonths(-3) },
             new Book { Id = 3, Title = "1984", Author = "George Orwell", CreatedDate = DateTime.Now.AddMonths(-1) },
-            new Book { Id = 4, Title = "Fogo & Sangue", Author = "Geaorge R.R. Martin", CreatedDate = DateTime.Now.AddMonths(-8) },
-            new Book { Id = 5, Title = "O Acerto de Contas de Uma Mãe", Author = "Sue Klebold", CreatedDate = DateTime.Now.AddMonths(-2) },
-            new Book { Id = 6, Title = "Poesias", Author = "Edgar Allan Poe", CreatedDate = DateTime.Now.AddMonths(-1) }
+            new Book { Id = 1, Title = "Fogo & Sangue", Author = "Geaorge R.R. Martin", CreatedDate = DateTime.Now.AddMonths(-8) },
+            new Book { Id = 2, Title = "O Acerto de Contas de Uma Mãe", Author = "Sue Klebold", CreatedDate = DateTime.Now.AddMonths(-2) },
+            new Book { Id = 3, Title = "Poesias", Author = "Edgar Allan Poe", CreatedDate = DateTime.Now.AddMonths(-1) }
         );
 
         base.OnModelCreating(modelBuilder);
